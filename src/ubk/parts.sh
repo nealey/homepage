ed20a7611ee0536a8d6fe03a4d34d5543ddd8b30
Neale Pickett <neale@woozle.org>
2011-10-03 17:51:00 -0500

    Add DJ page, do some cleanup

--- src/ubk/parts.sh
+++ src/ubk/parts.sh
@@ -2,13 +2,15 @@
 
 GIT_DIR=$HOME/projects/ubk; export GIT_DIR
 
+echo "<ul>"
 git ls-tree HEAD | while read mode type sum fn; do 
     case $fn in
 	*.ps)
 	    base=${fn%.ps}
 	    desc=$(git cat-file blob $sum | sed -n 's/^%.*Description: //p')
-	    test -n "$desc" || desc=$base
-	    echo "* [${desc:$base}](${base}.pdf)"
+	    [ -z "$desc" ] && desc="$base"
+	    echo "<li><a href="$base.pdf">${desc:-$base}</a></li>"
 	    ;; 
     esac
 done
+echo "</ul>"
