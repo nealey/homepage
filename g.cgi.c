1d914d777af8d67f8407f9bfbe709d52d965069c
Neale Pickett <neale@woozle.org>
2012-11-07 18:11:45 -0600

    eris release + faster git cloning

--- g.cgi.c
+++ g.cgi.c
@@ -1,11 +1,23 @@
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+#define GIT_PROJECT_ROOT "/home/neale/projects"
+#define CGIT_CONFIG "/home/neale/public_html/cgitrc"
 
 int
 main(int argc, char *argv[])
 {
-    setenv("CGIT_CONFIG", "/home/neale/public_html/cgitrc", 1);
-    execl("/usr/local/bin/cgit", "cgit", NULL);
+    char *uri = getenv("REQUEST_URI");
+
+    if (uri && strstr(uri, "git-upload-pack")) {
+        /* Use git-http-backend for great speed! */
+        setenv("GIT_PROJECT_ROOT", GIT_PROJECT_ROOT, 1);
+        execlp("git", "git", "http-backend", NULL);
+    } else {
+        setenv("CGIT_CONFIG", CGIT_CONFIG, 1);
+        execl("/usr/local/bin/cgit", "cgit", NULL);
+    }
 
     return 0;
 }
