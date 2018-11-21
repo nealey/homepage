b24a63b5c8e312cc7afb879c239e717de803a724
Neale Pickett <neale@woozle.org>
2018-11-21 20:19:31 +0000

    Start trying to do some PWA action

---
+++ toys/starship/starship.js
@@ -0,0 +1,62 @@
+function whiteNoise(audioCtx) {
+  var bufferSize = 17 * audioCtx.sampleRate,
+  noiseBuffer = audioCtx.createBuffer(1, bufferSize, audioCtx.sampleRate),
+  output = noiseBuffer.getChannelData(0);
+  for (var i = 0; i < bufferSize; i++) {
+    output[i] = Math.random() * 2 - 1;
+  }
+  var whiteNoise = audioCtx.createBufferSource();
+  whiteNoise.buffer = noiseBuffer;
+  whiteNoise.loop = true;
+  whiteNoise.start(0);
+  return whiteNoise;
+}
+
+function bandpassFilter(audioCtx, freq, gain) {
+  var filt = audioCtx.createBiquadFilter();
+  filt.type = "bandpass";
+  filt.frequency.value = freq;
+  filt.gain.value = gain;
+  return filt;
+}
+
+function init() {
+  var audioCtx = new window.AudioContext();
+  var synth = whiteNoise(audioCtx);
+  var filterA = bandpassFilter(audioCtx, 100, 20);
+  var filterB = bandpassFilter(audioCtx, 50, 20);
+  var gainA = audioCtx.createGain();
+
+  whiteNoise(audioCtx).connect(filterA);
+  filterA.connect(filterB);
+  filterB.connect(gainA);
+  gainA.connect(audioCtx.destination);
+  audioCtx.suspend();
+  
+  function setFade() {
+    var faderPos = document.querySelector("#fader").value;
+    gainA.gain.value = faderPos;
+  }
+  document.querySelector("#fader").addEventListener("input", setFade);
+  setFade();
+
+  document.querySelector("#play").addEventListener("click", e => {
+    if (audioCtx.state == "running") {
+      audioCtx.suspend();
+    } else {
+      audioCtx.resume();
+    }
+  });
+  
+  console.log(navigator.serviceWorker);
+  console.log("moo");
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("sw.js");
+  }
+}
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", init);
+} else {
+  init();
+}
---
+++ toys/starship/sw.js
@@ -0,0 +1,26 @@
+var cacheName = "starship-v1";
+var content = [
+  "index.html",
+  "starship.js"
+];
+
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.Open(cacheName).then(cache => cache.addAll(content)));
+});
+
+// Have mercy, this is a horror show
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => {
+      console.log("[Service Worker] Fetching resource:", e.request.url);
+      return r || fetch(e.request).then(response => {
+        return caches.open(cacheName).then(cache => {
+          console.log("[Service Worker] Caching new resource:", e.request.url);
+          cache.put(e.request, respone.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
