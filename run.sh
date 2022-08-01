188aca58ce317010e041fc0ddcba518181891096
Neale Pickett <neale@woozle.org>
2022-08-01 16:33:37 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 
-docker run --rm --net=host -v $(pwd):/srv/jekyll -u $(id -u):$(id -g) jekyll/jekyll:minimal /usr/gem/bin/jekyll s --host 0 "$@"
+cd $(dirname $0)
+docker run --rm -it -v $(pwd):/srv/jekyll -p 4000:4000 -e JEKYLL_UID=1000 -e JEKYLL_GID=1000 jekyll/jekyll jekyll build --watch
